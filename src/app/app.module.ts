9f162a293d75b69694591178902606b26598f08a
Lucas Bortolazzo <lucas.dolberth@yahoo.com>
2021-06-17 20:56:37 -0300

    adicionado todos os apps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { ServicosDisponiveisComponent } from './servicos-disponiveis/servicos-disponiveis.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    ServicosDisponiveisComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/servicos-disponiveis/servicos-disponiveis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servicos-disponiveis',
+  templateUrl: './servicos-disponiveis.component.html',
+  styleUrls: ['./servicos-disponiveis.component.css']
+})
+export class ServicosDisponiveisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
