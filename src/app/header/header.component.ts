ab06a68366aaa79fdf194c7d2b20a25e3377940e
Lucas Bortolazzo <lucas.dolberth@yahoo.com>
2021-06-17 23:39:20 -0300

    Adicionado pre-cadastro

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public rotaAtualEhHome: boolean = false;
+
+  private subscription: Subscription = new Subscription();
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.inicializarVariaveisTemplate();
   }
 
+  private inicializarVariaveisTemplate(): void {
+    this.subscription.add(
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationStart) {
+          this.rotaAtualEhHome = event.url === '/';
+        }
+      }))
+  };
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
